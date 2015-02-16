6b25d04114bd06ceefaf531be0e8a475c6406044
Alex Newman <thedotmack@gmail.com>
2015-02-15 20:05:14 -0500

    Grunt Demo Source

---
+++ Gruntfile.js
@@ -0,0 +1,119 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		connect: {
+			server: {
+				options: {
+	                open: true,
+	                livereload: true
+				}
+			}
+	    },
+		less: {
+			options: {
+				compress: true,
+				yuicompress: true,
+				optimization: 2,
+				sourceMap: true
+			},
+			style: {
+				files: {
+					'assets/css/main.min.css': 'assets/less/main.less'
+				}
+			}
+		},
+		autoprefixer: {
+			options: {
+				map: true,
+			},
+			main: {
+				src: 'assets/css/main.min.css',
+			},
+		},
+        // To use TinyPNG, get an API key from https://tinypng.com/developers
+        tinypng: {
+        	options: {
+        		apiKey: 'YOUR_API_KEY_HERE',
+        		checkSigs: true,
+        		sigFile: 'assets/images/file_sigs.json',
+        		summarize: true,
+        		showProgress: true
+        	},
+        	compress: {
+        		src: '*.png',
+        		cwd: 'assets/images/',
+        		dest: 'assets/images/',
+        		expand: true
+        	}
+        },
+        uglify: {
+            options: {
+                mangle: true,
+                compress: true,
+                preserveComments: 'some',
+                sourceMap: true,
+            },
+            js: {
+                files: {
+                    'assets/js/main.min.js': ['assets/js/main.js']
+                }
+            }
+        },
+		jade: {
+			dist: {
+			    options: {
+					data: function() {
+						return {
+							pictures: require( __dirname + '/assets/data/pictures.json')
+						}
+					}
+			    },
+			    files: {
+			      "index.html": "assets/templates/index.jade",
+			    }
+			}
+		},              
+		watch: {
+			js: {
+				files: [ 'assets/js/*.js' ],
+				tasks: [ 'uglify:js', 'jshint:all' ],
+				options: {
+					livereload: true,
+				}
+			},
+			css: {
+				files: [ 'assets/less/*.less' ],
+				tasks: [ 'less:style', 'autoprefixer:main' ],
+				options: {
+					livereload: true,
+				}
+			},
+			png: {
+				files: [ 'assets/images/*.png' ],
+				tasks: [ 'tinypng' ]
+			},
+			png: {
+				files: [ 'assets/templates/*.jade' ],
+				tasks: [ 'jade' ],
+				options: {
+					livereload: true,
+				}				
+			}
+		}
+    });
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');	
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-tinypng');
+	grunt.loadNpmTasks('grunt-contrib-jade');	
+
+	// Default task.
+	grunt.registerTask('serve', ['connect','watch']);
+
+};
